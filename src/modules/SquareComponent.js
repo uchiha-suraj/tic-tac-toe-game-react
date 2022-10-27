df7103787067b00fefdb0bb177343cd45b28d491
suraj <surajadhikarycs@gmail.com>
2022-10-27 09:52:43 +0530

    new change

--- src/modules/SquareComponent.js
+++ src/modules/SquareComponent.js
@@ -3,16 +3,16 @@ import React from 'react'
 const SquareComponent = (props) => {
   const classes = (props.className ? `${props.className} square` : `square`)
   return (
-    <span
-      className={classes}
-      onClick={() => props.onClick(props.index)}
-    >
-      {props.state}
-    </span><span
-       
-    >
-      {props.state}
-    </span>
+    <>
+      <span
+        className={classes}
+        onClick={() => props.onClick(props.index)}
+      >
+        {props.state}
+      </span>
+
+      {/* just checking */}
+    </>
   )
 }
 
